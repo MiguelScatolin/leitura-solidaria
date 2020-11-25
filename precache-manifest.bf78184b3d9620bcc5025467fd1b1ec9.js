b769ddcfca25719caf76a4339a8d4cef3b78474e
Miguel Scatolin <miguel.scatolin@gmail.com>
2020-11-24 23:47:27 -0300

    Updates

---
+++ precache-manifest.bf78184b3d9620bcc5025467fd1b1ec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5a10b79830e444a022054a578bd858",
+    "url": "/leitura-solidaria/index.html"
+  },
+  {
+    "revision": "fc7e26bfbc5954e94ae5",
+    "url": "/leitura-solidaria/static/css/main.76fe27f7.chunk.css"
+  },
+  {
+    "revision": "23f834b67d9f96041c27",
+    "url": "/leitura-solidaria/static/js/2.5d60cc76.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/leitura-solidaria/static/js/2.5d60cc76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea74132f4024d4f559d6",
+    "url": "/leitura-solidaria/static/js/3.5592a1e6.chunk.js"
+  },
+  {
+    "revision": "fc7e26bfbc5954e94ae5",
+    "url": "/leitura-solidaria/static/js/main.5f0c3d46.chunk.js"
+  },
+  {
+    "revision": "e242e0c5713c4ef84c41",
+    "url": "/leitura-solidaria/static/js/runtime-main.3cb4b114.js"
+  }
+]);
