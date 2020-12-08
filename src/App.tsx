dbffd28a714fc8badba2f8f75d689be0fc1d0c83
Miguel Scatolin <miguel.scatolin@gmail.com>
2020-12-08 00:18:48 -0300

    FIT: teste formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { TextForm } from "./features/text";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 function App() {
     return (
-        <MuiThemeProvider>
-            <Router>
-                <Switch>
-                    <Route path="/request">
-                        <TextForm />
-                    </Route>
-                    <Route path="/">
-                        <TextForm />
-                    </Route>
-                </Switch>
-            </Router>
-        </MuiThemeProvider>
+        <Router>
+            <Switch>
+                <Route path="/request">
+                    <TextForm />
+                </Route>
+                <Route path="/">
+                    <TextForm />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
--- src/features/text/views/TextForm.tsx
+++ src/features/text/views/TextForm.tsx
@@ -1,8 +1,79 @@
+import { Grid, MenuItem, Paper, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import { Paper } from "material-ui";
+
+const useStyles = makeStyles({
+    root: {
+        height: "100vh",
+        width: "100vw",
+    },
+});
 
 const TextForm = () => {
-    return <Paper>Formulário criação de texto</Paper>;
+    const classes = useStyles();
+    /* name: string;
+    authors: string[];
+    edition?: number;
+    language: string;
+    type: TextType;
+    hasImages: boolean; */
+    return (
+        <Paper className={classes.root}>
+            <Grid container direction="column" xs={12}>
+                Formulário de cadastro de texto
+                <Grid item>
+                    <TextField
+                        id="outlined-basic"
+                        label="Nome"
+                        aria-label="Nome do texto"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        id="outlined-basic"
+                        label="Autores"
+                        aria-label="Nome dos Autores"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        id="outlined-basic"
+                        label="Autores"
+                        aria-label="Nome dos Autores"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        id="lingua"
+                        label="Língua"
+                        value="20"
+                        select
+                        variant="outlined"
+                        aria-label="Língua do texto"
+                    >
+                        <MenuItem value="portugues10">Ten</MenuItem>
+                        <MenuItem value="ingles">Inglês</MenuItem>
+                    </TextField>
+                </Grid>
+                <Grid item>
+                    <TextField
+                        id="tipo"
+                        label="Tipo"
+                        value="20"
+                        select
+                        aria-label="Tipo do texto"
+                        variant="outlined"
+                    >
+                        <MenuItem value="Literatura">Literatura</MenuItem>
+                        <MenuItem value="artigo">Artigo</MenuItem>
+                    </TextField>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
 };
 
 export default TextForm;
