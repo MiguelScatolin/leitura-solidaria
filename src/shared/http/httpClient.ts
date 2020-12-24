57ae4fed96b94c104218484c8453192b958d304c
Miguel Scatolin <miguel.scatolin@gmail.com>
2020-12-23 23:37:54 -0300

    FIT: alterando endereço da API

--- src/shared/http/httpClient.ts
+++ src/shared/http/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: "https://leitor-yara.rj.r.appspot.com/",
+    baseURL: "http://35.247.35.37:5000/",
     responseType: "json",
     headers: { "content-type": "application/json" },
     timeout: 120000,
