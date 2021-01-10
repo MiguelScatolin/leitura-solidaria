9d4e656817cc4ef54ccf035d02c738a4ff313df0
Miguel Scatolin <miguel.scatolin@gmail.com>
2021-01-10 20:38:51 -0300

    FIX: mudando para URL da api sem porta explicita

--- src/shared/http/httpClient.ts
+++ src/shared/http/httpClient.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: "http://35.247.35.37:5000/",
-    responseType: "json",
-    headers: { "content-type": "application/json" },
-    timeout: 120000,
+	baseURL: "http://35.247.35.37/",
+	responseType: "json",
+	headers: { "content-type": "application/json" },
+	timeout: 120000,
 });
 
 export default httpClient;
